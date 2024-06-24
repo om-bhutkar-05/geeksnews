3000183f3c42c0cfaca88a583d421fbcd1595ea4
om <ombhutkar9@gmail.com>
2024-06-24 22:41:47 +0530

    added few features

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,11 +6,11 @@ export class NewsItem extends Component {
     return (
       <>
       <div className="card my-4" style={{width:"18rem"}}>
-  <img src={Url} className="card-img-top" alt='/' />
+  <img src={Url}  className="card-img-top" alt=".../public/default.png" />
   <div className="card-body">
-    <h5 className="card-title">{title}</h5>
-    <p className="card-text">{desc}</p>
-    <a href={newsid} className="btn btn-primary">Go somewhere</a>
+    <h5 className="card-title">{title}....</h5>
+    <p className="card-text">{desc}....</p>
+    <a href={newsid}  rel="noreferrer" target="_blank" className="btn btn-primary">open news</a>
   </div>
 </div>
       </>
